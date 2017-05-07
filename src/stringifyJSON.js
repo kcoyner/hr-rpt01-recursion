34dd01f931ac77006801476593a3ab4851423129
Kevin Coyner <kevin@rustybear.com>
2017-05-07 15:21:52 -0400

    doodle: Snapshot for partial completion of stringifyJSON.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,13 +4,57 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  if (isObject(obj)) {
-    return undefined;
-  } else {
-    return '9';
+  if (Object(obj) && Object.prototype.toString.call(obj) !== '[object Array]'){
+    return stringifyObj(obj);
+  } else if (Object.prototype.toString.call(obj) === "[object Null]"){
+    return "null";
+  } else if (Object.prototype.toString.call(obj) === "[object String]") {
+    return `"${obj.toString()}"`;
+  } else if (Object.prototype.toString.call(obj) === "[object Boolean]") {
+    return obj.toString();
+  } else if (Object.prototype.toString.call(obj) === "[object Number]") {
+    return obj.toString();
+  } else if (Object.prototype.toString.call(obj) === "[object Array]") {
+    return stringifyArr(obj);
   }
 };
 
-function isObject(obj) {
-  return obj === Object(obj) && Object.prototype.toString.call(obj) !== '[object Array]'
+
+var foo = [1,2];
+var foo = {'a': 'apple'};
+var foo = ['1','2'];
+var foo =  [[[[[[['abc']]]]]]];
+
+console.log(stringifyJSON(foo));
+console.log(JSON.stringify(foo));
+
+function stringifyArr (obj) {
+  let pre = '[';
+  let post = ']';
+  let str = '';
+  for (let item of obj) {
+    str += stringifyJSON(item);
+    str = `${str},`;
+  }
+  str = str.slice(0, str.length - 1);
+  return pre + str + post;
 }
+
+function stringifyObj (obj) {
+  let pre = '{';
+  let post = '}';
+  var strKey = '';
+  var strVal = '';
+  // console.log('obj: ', obj);
+  for (var x in obj) {
+    // console.log('x: ', x);
+    // console.log('obj[x]: ', obj[x]);
+    // strKey = stringifyJSON(x);
+    strVal += stringifyJSON(obj.x);
+    console.log('strVal: ', strVal);
+  }
+  return pre + strKey + strVal + post;
+}
+
+
+
